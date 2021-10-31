d63c12924c9a3af0dd63b156f3948de45f3c4f09
Navhal Ritesh <ylight528@gmail.com>
2021-10-31 17:55:21 +0530

    V4.0

--- Package.swift
+++ Package.swift
@@ -23,7 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Klapz",
-            path: "Sources",
+            dependencies: [
+                .product(name: "CountryPickerView", package: "CountryPickerView"),
+                .product(name: "MultilineTextField", package: "MultilineTextField"),
+                .product(name: "NKVPhonePicker", package: "NKVPhonePicker")
+            ],
             resources: [.copy("images/wihteklapz.png"),.copy("images/klapzicon.png"),.copy("images/noti.png"),.copy("images/klap2.png"),.process("Resources") ]
             ),
     ]
