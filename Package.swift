842a6ba47e81a634bf5e74d9c47f18e9c50baf08
Navhal Ritesh <ylight528@gmail.com>
2021-12-01 09:39:41 +0530

    chnages

--- Package.swift
+++ Package.swift
@@ -28,7 +28,8 @@ let package = Package(
                 .product(name: "MultilineTextField", package: "MultilineTextField"),
                 .product(name: "NKVPhonePicker", package: "NKVPhonePicker")
             ],
-            resources: [.copy("images/wihteklapz.png"),.copy("images/klapzicon.png"),.copy("images/noti.png"),.copy("images/klap2.png"),.process("images/klapzicon.png") ,.process("images/noti.png") ]
+            path: "."
             ),
+          
     ]
 )
